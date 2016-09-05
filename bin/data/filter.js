17acbdaeb746fb3fb8a40f60989f30dfa8449cec
Olga Moiseyenko <olyamoiseyenko@gmail.com>
2016-09-05 15:01:54 +0200

    Filters with error

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -1,5 +1,39 @@
 'use strict';
 
 module.exports = function(list, filterID) {
-  return list;
+  var listFiltered;
+  var threeDays = 3 * 4 * 60 * 60 * 1000;
+  switch(filterID) {
+    case 'reviews-recent':
+      listFiltered = list.filter(function(item) {
+        var dateCurrent = new Date(item.created);
+        return Date.now() - dateCurrent <= threeDays;
+      });
+      listFiltered = listFiltered.sort(function(a, b) {
+        return b.created - a.created;
+      });
+      break;
+    case 'reviews-good':
+      listFiltered = list.filter(function(item) {
+        return item.rating >= 3;
+      });
+      listFiltered = listFiltered.sort(function(a, b) {
+        return b.rating - a.rating;
+      });
+      break;
+    case 'reviews-bad':
+      listFiltered = list.filter(function(item) {
+        return item.rating < 3;
+      });
+      listFiltered = listFiltered.sort(function(a, b) {
+        return b.rating - a.rating;
+      });
+      break;
+    case 'reviews-popular':
+      listFiltered = listFiltered.sort(function(a, b) {
+        return b.review_usefulness - a.review_usefulness;
+      });
+      break;
+  }
+  return listFiltered;
 };
--- src/load.js
+++ src/load.js
@@ -1,13 +1,16 @@
 'use strict';
 
-module.exports = (function(url, callback) {
-  var elem = document.createElement('script');
-  elem.src = url;
-  document.head.appendChild(elem);
-  var reviews = [];
+module.exports = (function(url, params, callback) {
+  var xhr = new XMLHttpRequest();
 
-  window.JSONPCallback = function(data) {
-    reviews = data;
+  xhr.onload = function(evt) {
+    var reviews = JSON.parse(evt.target.response);
     callback(reviews);
   };
+  xhr.open('GET', url +
+   '?from=' + (params.from || 0) +
+   '&to=' + (params.to || Infinity) +
+   '&filter=' + (params.filter || 'default'));
+
+  xhr.send();
 });
