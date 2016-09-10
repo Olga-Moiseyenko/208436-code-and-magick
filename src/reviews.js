dfbfe910f8df7ec9f04d695716bfe58c53635946
Olga Moiseyenko <olyamoiseyenko@gmail.com>
2016-09-11 00:44:39 +0200

    LocalStorage for filters

--- src/reviews.js
+++ src/reviews.js
@@ -6,10 +6,19 @@ var Review = require('./review');
 var reviewsList = document.querySelector('.reviews-list');
 var reviewsFilter = document.querySelector('.reviews-filter');
 var reviewsMore = document.querySelector('.reviews-controls-more');
+var lastSavedFilter = localStorage.getItem('lastFilter');
 
 var pageNumber = 0;
 var pageSize = 3;
-var currentFilter = 'all';
+var currentFilter;
+
+if(lastSavedFilter !== '') {
+  currentFilter = lastSavedFilter;
+  document.querySelector('#' + currentFilter).checked = true;
+} else {
+  currentFilter = 'all';
+}
+
 reviewsFilter.classList.add('invisible');
 
 var getReviews = function(reviews) {
@@ -32,6 +41,7 @@ var changeFilters = function(filterID) {
   currentFilter = filterID;
   pageNumber = 0;
   loadPageReviews(pageNumber++, currentFilter);
+  localStorage.setItem('lastFilter', currentFilter);
 };
 
 reviewsMore.addEventListener('click', function() {
@@ -39,9 +49,7 @@ reviewsMore.addEventListener('click', function() {
 });
 
 reviewsFilter.addEventListener('change', function(evt) {
-
   changeFilters(evt.target.id);
-
 });
 
 changeFilters(currentFilter);
